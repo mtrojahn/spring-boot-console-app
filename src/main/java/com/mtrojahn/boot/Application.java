79202344bd62ffbec1819b9847ddc49768e67dc2
Coderdood <mtrojahn@gmail.com>
2015-11-16 14:13:38 -0200

    Comments updated.

--- src/main/java/com/mtrojahn/boot/Application.java
+++ src/main/java/com/mtrojahn/boot/Application.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-/**
- * From the manual:
- *
+/*
+ * Manual:
  * public @interface SpringBootApplication
  * Indicates a configuration class that declares one or more @Bean methods and also triggers auto-configuration and
  * component scanning. This is a convenience annotation that is equivalent to declaring @Configuration,
  * @EnableAutoConfiguration and @ComponentScan.
+ *
+ * Registra uma classe de configuração que declara um ou mais metodos @Bean e também dispara a auto-configuração e
+ * busca por componentes neste pacote. Esta é uma anotação de conveniência que é equivalente a declarar @Configuration
+ * @EnableAutoConfiguration e @ComponentScan.
+ *
  */
 @SpringBootApplication
 public class Application {
