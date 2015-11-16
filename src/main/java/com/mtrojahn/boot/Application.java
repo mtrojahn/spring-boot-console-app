5164e7834696742c8dc802890ffbea900182a4ec
Coderdood <mtrojahn@gmail.com>
2015-11-16 14:02:40 -0200

    Initial commit

---
+++ src/main/java/com/mtrojahn/boot/Application.java
@@ -0,0 +1,32 @@
+package com.mtrojahn.boot;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * From the manual:
+ *
+ * public @interface SpringBootApplication
+ * Indicates a configuration class that declares one or more @Bean methods and also triggers auto-configuration and
+ * component scanning. This is a convenience annotation that is equivalent to declaring @Configuration,
+ * @EnableAutoConfiguration and @ComponentScan.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .sources(Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+
+        Application app = context.getBean(Application.class);
+        app.start();
+    }
+
+    private void start() {
+        System.out.println("Hello World!");
+    }
+}
